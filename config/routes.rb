d9c0bac1e8ddf6fbdb67c94644756bcfe8f1e3ab
akirahashibami <hashibami2207@gmail.com>
2020-07-22 07:35:13 +0000

    [Add] model controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'homes/index'
   get 'homes/show'
   devise_for :users
+
+  resources :books
+  resources :users, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
