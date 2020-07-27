c7a7f6ee6412439c0f4cc5ce44db2990aa1189e0
akirahashibami <hashibami2207@gmail.com>
2020-07-27 06:31:58 +0000

    [ReStart] Bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-
-  devise_for :users
-
-  root 'homes#index'
-
-  get 'homes/about' => 'homes/show'
-
-  resources :homes, only: [:index]
-  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
-  resources :users, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20200723101955_rename_introsuction_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameIntrosuctionColumnToUsers < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :users, :introsuction, :introduction
-  end
-end
