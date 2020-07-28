f09cc9fcde9244060837efdcfc03959db93471ce
akirahashibami <hashibami2207@gmail.com>
2020-07-28 09:49:33 +0000

    [Complate] comment

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,9 +1,9 @@
 class PostCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:id])
+    book = Book.find(params[:book_id])
     # bookに投稿するbookのデータを渡す
-    comment = PostCommnet.new(post_comment_params)
+    comment = PostComment.new(post_comment_params)
     # commentに、form_forで入力された内容を新規に渡す
     comment.user_id = current_user.id
     # commentのユーザーIDカラムに、ログインしている人のユーザーIDが渡される
@@ -14,9 +14,16 @@ class PostCommentsController < ApplicationController
   end
 
   def destroy
-    PostCommnet.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    comment = PostComment.find_by(id: params[:id], book_id: params[:book_id])
     # find_byは一通り呼び出して最初にヒットしたものを呼び出す。コメントIDとしてparams[:id]を、ブックIDとしてparams[:book_id]を呼び出している
+    comment.destroy
     redirect_to book_path(params[:book_id])
+  end
+
+  private
+
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
   end
 
 end
